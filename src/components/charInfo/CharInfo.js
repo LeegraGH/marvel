855b6f20be1bb9f975d64b6a33c3a7a79546727c
Anya <use.anya@gmail.com>
2023-08-31 13:14:40 +0300

    add propTypes

--- src/components/charInfo/CharInfo.js
+++ src/components/charInfo/CharInfo.js
@@ -1,4 +1,6 @@
 import { Component } from 'react';
+import PropTypes from 'prop-types';
+
 import MarvelService from '../../services/MarvelService';
 import Spinner from '../spinner/Spinner';
 import Skeleton from '../skeleton/Skeleton';
@@ -100,6 +102,10 @@ const View = ({char}) => {
             </ul>
         </>
     )
+}
+
+CharInfo.propTypes={
+    charId: PropTypes.number
 }
 
 export default CharInfo;
